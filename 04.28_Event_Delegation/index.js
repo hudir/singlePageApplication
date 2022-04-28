948ae628e74093122bcba63bb5c1cbf2ec19f4c9
hudir <hudirybw@gmail.com>
2022-04-28 11:37:49 +0200

    humm

---
+++ 04.28_Event_Delegation/index.js
@@ -0,0 +1,72 @@
+const tree=document.querySelectorAll('ul');
+const lis=tree[0].querySelectorAll('li');
+
+
+
+// put the text in a span tag in lis
+lis.forEach(el=>{
+    let span = document.createElement('span')
+    // span.innerText=el.firstChild
+    span.append(el.firstChild)
+    el.prepend(span)
+});
+
+// add listner to uls key= target.tagname ==span
+tree.forEach(el=>el.classList.add('d-block'))
+
+tree[0].addEventListener('click', (e)=>{
+    console.log(e.target.tagName);
+    // console.log(e.target.nextElementSibling.className);
+
+    
+    if(e.target.tagName == 'SPAN'){
+        // if (!e.target.firstChild) return
+
+        // let nestedList = e.target.parentNode.querySelector('ul')
+    // if(!nestedList) return
+
+    // nestedList.hidden = !nestedList.hidden
+
+        // console.log(Boolean(e.target.children[0])); this dont really work
+
+        if(e.target.nextElementSibling.nodeName=='UL' && e.target.nextElementSibling.className == 'd-none'){
+            e.target.nextElementSibling.classList.replace('d-none','d-block')
+        } 
+        
+        else if(e.target.nextElementSibling.nodeName=='UL'  && e.target.nextElementSibling.className == 'd-block'){
+            e.target.nextElementSibling.classList.replace('d-block','d-none')
+        }
+    }
+})
+
+
+
+
+
+let div = document.querySelector('div')
+
+let newSpan = document.createElement('span')
+
+// now take the msg away and put it in newSpan
+newSpan.append(div.firstChild)
+
+// now we put newSpan to the origin position use prepend
+// newSpan.innerText='here is the message, now in span tag'
+
+div.prepend(newSpan)
+
+// div.append(newSpan)
+
+console.log(newSpan);
+
+
+
+
+
+
+// that's the special case xD if no parent is there u get null with parentElement 
+// try document.querySelector('HTML').parentElement 
+// then try document.querySelector('HTML').nodeParent 
+
+console.log(document.querySelector('HTML').parentElement); //null
+console.log(document.querySelector('HTML').nodeParent );  // undefined
