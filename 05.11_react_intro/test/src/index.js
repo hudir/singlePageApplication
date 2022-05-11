e2ca171a980ce6e675102ff24d53072296f0173a
hudir <hudirybw@gmail.com>
2022-05-11 11:57:20 +0200

    ok

--- 05.11_react_intro/test/src/index.js
+++ 05.11_react_intro/test/src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+// let App = () =><h1>My first react app</h1> // JSX component first letter uppercase
+const namesInHTML = [
+  <li className='li1'>Bob</li>,
+  <li>Bob</li>,
+  <li>Bob</li>,
+  <li>Bob</li>
+];
+
+const content = <ul>{namesInHTML}</ul>
+
+
+root.render(content);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
