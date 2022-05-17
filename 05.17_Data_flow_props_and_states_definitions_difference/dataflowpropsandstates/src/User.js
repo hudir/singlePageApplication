62d0650c19335b0b9d5521b0a3ff872508161002
hudir <hudirybw@gmail.com>
2022-05-17 13:51:57 +0200

    store in state

--- 05.17_Data_flow_props_and_states_definitions_difference/dataflowpropsandstates/src/User.js
+++ 05.17_Data_flow_props_and_states_definitions_difference/dataflowpropsandstates/src/User.js
@@ -1,37 +1,68 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react';
 
-let data = [{userName: "john1", email: "qqqq", city: "wwwwww"}, {userName: "john2", email: "xxxx", city: "wwwwww"},{userName: "john3", email: "qqqq", city: "wwwwww"},{userName: "john", email: "qqqq", city: "wwwwww"},{userName: "john4", email: "qqqq", city: "wwwwww"},{userName: "max", email: "dfgdfgdfg", city: "hjh"}];
+// let i = 0;
 
-// let i = 0
+// export default function User() {
+
+//     let [users, setUser ] = useState([{userName: "john1", email: "qqqq", city: "wwwwww"}, {userName: "john2", email: "xxxx", city: "wwwwww"},{userName: "john3", email: "qqqq", city: "wwwwww"},{userName: "john", email: "qqqq", city: "wwwwww"},{userName: "john4", email: "qqqq", city: "wwwwww"},{userName: "max", email: "dfgdfgdfg", city: "hjh"}])
+   
+//    const eventFunction = e =>{
+//     // let i = user.indexOf(user[i])
+//      if(i===users.length-1) i=0
+//     else  i++
+//     setUser(users)
+//     console.log(i);
+//    }
+  
+//   return (
+//     <div onClick={eventFunction}>
+//         <div>next user</div>
+//         <h2>{users[i].userName}</h2>
+//         <h3>{users[i].email}</h3>
+//         <h4>{users[i].city}</h4>
+//     </div>
+//   )
+// }
+
+import React, { useState } from "react";
+
+// const users=[
+//     { username: "John", email: "qqqqqq", city: "wwwwwwwww" },
+//     { username: "rrrrr", email: "zzzzz", city: "oooooo" },
+//     { username: "ddddddd", email: "ffffff", city: "hhhhh" },
+//   ]
 
 export default function User() {
+    const [users, setUsers] = useState([
+        { username: "John", email: "qqqqqq", city: "wwwwwwwww" },
+        { username: "rrrrr", email: "zzzzz", city: "oooooo" },
+        { username: "ddddddd", email: "ffffff", city: "hhhhh" },
+      ])
+  const [user, setUser] = useState(users[0]);
 
-    
-    
-    console.log(Boolean(data === data)) 
 
-    let [user, setUser ] = useState(data[0])
-   
-    
+ const handleNext= ()=>{
 
-   const eventFunction = e =>{
-    let i = data.indexOf(user)
-    
-    console.log(user);
-    console.log(i);
+     let i = users.indexOf(user)
 
-    if(i===data.length-1) setUser(data[0])
-    else setUser(data[i + 1])
-
-    // i++
-   }
+     if (i === users.length - 1) {
+        setUser(users[0]);
+        } else {
+        setUser(users[i + 1]);
+        } 
+     
+ }
 
   return (
-    <div onClick={eventFunction}>
-        <div>next user</div>
-        <h2>{user.userName}</h2>
-        <h3>{user.email}</h3>
-        <h4>{user.city}</h4>
+    <div>
+    <div onClick={handleNext}>Next</div>
+        <div>
+          <h2> {user.username} </h2>
+          <h3> {user.email} </h3>
+          <h4> {user.city} </h4>
+          <hr/>
+        </div>
+     
     </div>
-  )
+  );
 }
