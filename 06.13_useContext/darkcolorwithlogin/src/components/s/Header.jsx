c06ea5502bef9e55ecfa853cf6a1edec03e11b44
hudir <hudirybw@gmail.com>
2022-06-13 11:52:28 +0200

    delete some files

--- 06.13_useContext/darkcolorwithlogin/src/components/s/Header.jsx
+++ 06.13_useContext/darkcolorwithlogin/src/components/s/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     const {theme, checkboxChangeHandler} =useContext(Context)
   return (
     <div>
-        <label htmlFor="color">{theme+" color "}</label>
+        <label htmlFor="color">{theme+" Mode "}</label>
         <input type="checkbox" name="switch" id="color" onChange={checkboxChangeHandler} defaultChecked/>
     </div>
   )
