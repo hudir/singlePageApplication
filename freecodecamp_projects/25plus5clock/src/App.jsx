50b23bf44f5b7fe64c337312a56205e0f7efb9bb
hudir <hudirybw@gmail.com>
2022-08-09 10:23:02 +0200

    looks good

--- freecodecamp_projects/25plus5clock/src/App.jsx
+++ freecodecamp_projects/25plus5clock/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 
 const toMMSS = num =>{
   if(!isNaN(+num)) {
-    return num+':00'
+    if(+num<10) return '0'+num+':00'
+    else return num+':00'
   }
   else return num
 }
@@ -32,30 +33,42 @@ function App() {
     if(run){
       // setTimeLeft running
       const interval = setInterval(()=>{
-        let min = timeLeft.split(':')[0]
-           ,sec = timeLeft.split(':')[1]
-           if(min > 0 || sec > 0){
+        let min = timeLeft.split(':')[0].split('').length >= 2 ? timeLeft.split(':')[0] : `0${timeLeft.split(':')[0]}`
+           ,sec = timeLeft.split(':')[1].split('').length >= 2 ? timeLeft.split(':')[1] : `0${timeLeft.split(':')[1]}`
+           if(min > 0 || sec > 1){
             if(sec > 0) sec--;
             else {
               min--;
-              sec = 60
-            } 
+              sec = 59
+            }
+            setTimeLeft(min+':'+sec) 
            } else {
-            // change currentSession
+            const audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3')
+             audio.play()
+            // change currentSession and play the sound
+            if(currentSession == 'Session') {
+              setCurrentSession('Break');
+              setTimeLeft(toMMSS(breakLength))
+            }  else {
+              setCurrentSession('Session')
+              setTimeLeft(toMMSS(sessionLength))
+            }
           }
+      }, 1000)
+      return () => clearInterval(interval);
+    }
+  },[run, timeLeft])
 
-      })
-    } else {
+  useEffect(()=>{
       // update the Length to timeleft
       if(currentSession === 'Session'){
         setTimeLeft(toMMSS(sessionLength))
       } else if (currentSession === 'Break'){
         setTimeLeft(toMMSS(breakLength))
       }
-    }
     
-
-  },[sessionLength, breakLength, run])
+ 
+  },[sessionLength, breakLength,])
 
 
 
@@ -65,12 +78,12 @@ function App() {
 
         <div className="break">
           <button id="break-increment" onClick={e=>{
-            if(breakLength<60)
+            if(breakLength<60 && !run)
             setBreakLength(p=>+p+1)
           }}>+</button>
           <span id="break-label">Break Length:  </span>
           <button id="break-decrement" onClick={e=>{
-            if(breakLength>1)
+            if(breakLength>1 && !run)
             setBreakLength(p=>p-1)
           }}>-</button>
           <p id="break-length">{breakLength}</p>
@@ -79,12 +92,12 @@ function App() {
 
         <div className="session">
           <button id="session-increment" onClick={e=>{
-            if(sessionLength<60)
+            if(sessionLength<60 && !run)
              setSessionLength(p=>+p+1)
           }}>+</button>
           <span id="session-label">Session Length</span>
           <button id="session-decrement" onClick={e=>{
-            if(sessionLength>1)
+            if(sessionLength>1 && !run)
              setSessionLength(p=>p-1)
           }}>-</button>
           <p id="session-length">{sessionLength}</p>
